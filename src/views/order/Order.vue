045312d12b36515558e20932e66002a53f186ba3
fkrkaktk <68282411+fkrkaktk@users.noreply.github.com>
2021-11-23 17:51:30 +0900

    김원준 20211123

--- src/views/order/Order.vue
+++ src/views/order/Order.vue
@@ -22,7 +22,7 @@
           </div>
           <div class='deAddInput'>
             <div class="field">
-              <input type="button" class="btnAddressSearch" value="주소찾기">
+              <input type="button" class="btnAddressSearch" value="주소찾기" @click="daumaddress">
               <!-- <button class="btnAddressSearch">주소찾기</button> -->
               <input type="text" class="codeAddress" placeholder="우편번호">
             </div>
@@ -97,258 +97,166 @@
 
     <div class="cartBtnBox">
       <button class="fillBtn">결제하기</button>
+      <button class="fillBtn" @click="kakaopay">카카오 테스트</button>
+      <button class="fillBtn" @click="danalss">다날 테스트</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
-};
+export default {
+  data() {
+    return {
+      zip: '',
+      addr1: '',
+      addr2: ''
+    };
+  },
+  methods: {
+    kakaopay() {
+      var IMP = window.IMP; // 생략가능
+      IMP.init('imp50325691');
+      // 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
+      // i'mport 관리자 페이지 -> 내정보 -> 가맹점식별코드
+      IMP.request_pay({
+      pg: 'kakaopay', // version 1.1.0부터 지원.
+      /*
+      'kakaopay':카카오페이,
+      html5_inicis':이니시스(웹표준결제)
+      'nice':나이스페이
+      'jtnet':제이티넷
+      'uplus':LG유플러스
+      'danal':다날
+      'payco':페이코
+      'syrup':시럽페이
+      'paypal':페이팔
+      */
+      pay_method: 'card',
+      /*
+      'samsung':삼성페이,
+      'card':신용카드,
+      'trans':실시간계좌이체,
+      'vbank':가상계좌,
+      'phone':휴대폰소액결제
+      */
+      merchant_uid: 'merchant_' + new Date().getTime(),
+      /*
+      merchant_uid에 경우
+      https://docs.iamport.kr/implementation/payment
+      위에 url에 따라가시면 넣을 수 있는 방법이 있습니다.
+      참고하세요.
+      나중에 포스팅 해볼게요.
+      */
+      name: '주문명:결제테스트',
+      //결제창에서 보여질 이름
+      amount: 1000,
+      //가격
+      buyer_email: 'iamport@siot.do',
+      buyer_name: '구매자이름',
+      buyer_tel: '010-1234-5678',
+      buyer_addr: '서울특별시 강남구 삼성동',
+      buyer_postcode: '123-456',
+      m_redirect_url: 'http://localhost:9090/payments/complete'
+      /*
+      모바일 결제시,
+      결제가 끝나고 랜딩되는 URL을 지정
+      (카카오페이, 페이코, 다날의 경우는 필요없음. PC와 마찬가지로 callback함수로 결과가 떨어짐)
+      */
+      }, function (rsp) {
+      console.log(rsp);
+      if (rsp.success) {
+      var msg = '결제가 완료되었습니다.';
+      msg += '고유ID : ' + rsp.imp_uid;
+      msg += '상점 거래ID : ' + rsp.merchant_uid;
+      msg += '결제 금액 : ' + rsp.paid_amount;
+      msg += '카드 승인번호 : ' + rsp.apply_num;
+      } else {
+      var msg = '결제에 실패하였습니다.';
+      msg += '에러내용 : ' + rsp.error_msg;
+      }
+      alert(msg);
+      });
+    },
+    danalss() {
+      var IMP = window.IMP; // 생략가능
+      console.log(IMP);
+      IMP.init('imp50325691');
+      IMP.request_pay({
+      // pg: 'danal.9810030929',
+      pay_method: 'card',
+      merchant_uid: 'product_' + new Date().getTime(),
+      name: '주문명:Desker',
+      //결제창에서 보여질 이름
+      amount: 1000,
+      //가격
+      buyer_email: 'iamport@siot.do',
+      buyer_name: '흠바스키',
+      buyer_tel: '010-1234-5678',
+      buyer_addr: '부산 광역시 강남구 삼성동',
+      buyer_postcode: '123-456',
+      m_redirect_url: 'http://localhost:9090/payments/complete'
+      /*
+      모바일 결제시,
+      결제가 끝나고 랜딩되는 URL을 지정
+      (카카오페이, 페이코, 다날의 경우는 필요없음. PC와 마찬가지로 callback함수로 결과가 떨어짐)
+      */
+      }, function (rsp) {
+      console.log(rsp);
+      if (rsp.success) {
+      var msg = '결제가 완료되었습니다.';
+      msg += '고유ID : ' + rsp.imp_uid;
+      msg += '상점 거래ID : ' + rsp.merchant_uid;
+      msg += '결제 금액 : ' + rsp.paid_amount;
+      msg += '카드 승인번호 : ' + rsp.apply_num;
+      } else {
+      var msg = '결제에 실패하였습니다.';
+      msg += '에러내용 : ' + rsp.error_msg;
+      }
+      alert(msg);
+      });
+    },
+    daumaddress() {
+      new daum.Postcode({
+        oncomplete: function(data) {
+          // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분. 
+          // 도로명 주소의 노출 규칙에 따라 주소를 조합한다. 
+          // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+          let fullRoadAddr = ''; 
+          if(data.userSelectedType == 'R'){
+            fullRoadAddr = data.roadAddress; // 도로명 주소 변수 
+          }
+          else {
+            fullRoadAddr = data.jibunAddress; // 지번 주소 변수
+          }
+          let extraRoadAddr = ''; // 도로명 조합형 주소 변수 
+          // 법정동명이 있을 경우 추가한다. (법정리는 제외) 
+          // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다. 
+          if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)) { extraRoadAddr += data.bname; } 
+          // 건물명이 있고, 공동주택일 경우 추가한다. 
+          if(data.buildingName !== '' || data.apartment === 'Y') { 
+            extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName); 
+          } 
+          // 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다. 
+          if(extraRoadAddr !== '') { extraRoadAddr = ' (' + extraRoadAddr + ')'; } 
+          // 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다. 
+          if(fullRoadAddr !== '') { fullRoadAddr += extraRoadAddr; } 
+          // 우편번호와 주소 정보를 해당 필드에 넣는다. 
+          this.zip = data.zonecode; //5자리 새우편번호 사용 
+          this.addr1 = fullRoadAddr;
+          console.log(data);
+          console.log(this.zip);
+          console.log(this.addr1);
+          console.log(extraRoadAddr);
+        }
+    }).open();
+    }
+  },
+}
 </script>
 
 <style lang='scss' scoped>
+@import '@/assets/scss/order/order.scss';
 
-.orderBox {
-  width: 1200px;
-  margin-bottom: 45px;
-  margin: 0 auto;
-  // position: relative;
-}
-.orderTitleCon {
-  // background: #eee;
-  padding: 26px 0px 16px 0px;
-  border-bottom: 1px solid #777;
-}
-.orderTitle {
-  height: 40px;
-  h2 {
-    float: left;
-    font-size: 18px;
-    color: #999;
-    line-height: 40px;
-  }
-  .deliveryBtn {
-    margin-left: 25px;
-    display: inline-block;
-    padding: 9px 11px 8px;
-    border: 1px solid #ccc;
-    background: #fff;
-    vertical-align: middle;
-    line-height: 1.4;
-    font-size: 15px;
-    color: #111;
-    text-align: center;
-    box-sizing: border-box;
-    cursor: pointer;
-  }
-}
-.field {
-  margin-bottom: 15px;
-  input[type='text'] {
-    display: block;
-    float: left;
-    width: 608px;
-    height: 46px;
-    padding: 13px 16px;
-    border: 1px solid #ccc;
-    font-size: 15px;
-    font-weight: normal;
-    line-height: 20px;
-    color: #111;
-    letter-spacing: 0;
-    &::placeholder {
-      color: #999;
-    }
-  }
-  &:after {
-    display: block;
-    content: "";
-    clear: both;
-  }
-}
-.deliveryBox {
-  position: relative;
-  margin-top: 20px;
-}
-.deName {
-  display: block;
-  .receiver {
-    label {
-      font-size: 18px;
-      line-height: 24px;
-      font-weight: bold;
-      color: #111;
-    }
-    .receivername {
-      width: 341px;
-    }
-  }
-}
-.deAddInput {
-  .btnAddressSearch {
-    float: left;
-    width: 108px;
-    height: 46px;
-    margin-right: 15px;
-    border: 1px solid #f43142;
-    font-size: 15px;
-    line-height: 46px;
-    color: #f43142;
-    text-align: center;
-    letter-spacing: normal;
-    text-decoration: none;
-    background-color: #fff;
-    cursor: pointer;
-  }
-  .field{
-    .codeAddress { 
-      width: 218px;
-    }
-  }
-}
-.dePhoneInput {
-  .field {
-    input {
-      width: 234px;
-      margin-left: 15px;
-    }
-    .firstnum {
-      width: 110px;
-      margin: 0;
-    }
-  }
-}
-
-
-.cartTB {
-  width: 1200px;
-  margin: 20px auto;
-  text-align: center;
-  border-collapse: collapse;
-  thead {
-    tr {
-      th {
-        padding: 14px 20px;
-        border-bottom: 1px solid #eee;
-        text-align: center;
-      }
-    }
-  }
-  tbody {
-    .imgCon {
-      width: 200px;
-    }
-    tr {
-      border-top: 1px solid #eee;
-      td {
-        padding: 20px 0px;
-        .cartImgBox {
-          width: 250px;
-          height: 250px;
-          padding: 20px 0 0 0;
-          margin-left: 10px;
-          img {
-            width: 100%;
-            height: 100%;
-          }
-        }
-      }
-    }
-  }
-}
-
-.TitleCon {
-  // background: #eee;
-  padding: 26px 0px 16px 0px;
-  border-bottom: 1px solid #999;
-  margin-bottom: 15px;
-}
-.Title {
-  height: 40px;
-  h2 {
-    float: left;
-    font-size: 20px;
-    line-height: 40px;
-  }
-}
-.cartPriceCon {
-  .cartPrice {
-    float: right;
-    margin-right: 100px;
-    h4 {
-      float: left;
-      font-size: 2em;
-      font-weight: bold;
-      // line-height: 22px;
-      color: #f43142;
-    }
-    .price {
-      float: left;
-      display: block;
-      padding-left: 40px;
-      font-size: 15px;
-      color: #f43142;
-      text-align: right;
-      .totalPrice {
-        margin-right: 3px;
-        font-size: 34px;
-        font-weight: bold;
-        line-height: 41px;
-        vertical-align: -3px;
-        font-style: normal;
-      }
-    }
-    &:after {
-      display: block;
-      content: '';
-      clear: both;
-    }
-  }
-  &:after {
-    display: block;
-    content: '';
-    clear: both;
-  }
-}
-
-.cartBtnBox {
-  text-align: center;
-  margin-top: 15px;
-}
-.fillBtn {
-  font-size: 25px;
-  padding: 5px 40px;
-  border: 2px solid #FF6B95;
-  background-color: transparent;
-  color: #FF6B95;
-  text-transform: uppercase;
-  letter-spacing: 5px;
-  font-weight: bold;
-  position: relative;
-  transition: all 0.4s;
-  overflow: hidden;
-  margin-right: 10px;
-}
-.fillBtn:focus {
-  outline: none;
-}
-.fillBtn::before {
-  content: "";
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  background-image: linear-gradient(-45deg, #FFC796 0%, #FF6B95 100%);
-  top: 0%;
-  left: -100%;
-  transition: all 0.4s;
-  z-index: -1;
-}
-.fillBtn:hover::before {
-  transform: translateX(100%);
-}
-.fillBtn:hover {
-  color: #eeee;
-  // color: #fff;
-}
 </style>
